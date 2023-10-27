e670d65bfd899876be2a6e3beaa46c34d3aa45ff
xjccc <546534045@qq.com>
2023-10-27 15:26:40 +0800

    ofetch

---
+++ src/pages/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    result: {{ result }}
+    <br />
+    error: {{ e }}
+    <button @click="getData">fetch</button>
+  </div>
+</template>
+
+<script setup>
+// Reproduction
+import { ofetch } from 'ofetch'
+import { ref } from 'vue'
+const result = ref()
+const e = ref()
+const getData = async () => {
+  try {
+    const data = await ofetch('/api/info')
+    result.value = data
+    console.log(data)
+  } catch (error) {
+    e.value = error.message
+  }
+}
+</script>
