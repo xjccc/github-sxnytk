858077eb6f7703638960205a36b7839e0225ef33
xjccc <546534045@qq.com>
2023-10-30 09:49:23 +0800

    add: vconsole

--- src/entry-client.ts
+++ src/entry-client.ts
@@ -1,6 +1,8 @@
 import { createApp } from './main';
-
+import VConsole from 'vconsole';
 const { app, router } = createApp();
+
+const vConsole = new VConsole()
 
 router.isReady().then(() => {
   app.mount('#app');
